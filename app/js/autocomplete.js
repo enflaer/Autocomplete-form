9e69daf97b36e942aa37280ded3dd6985c65e432
enflaer <enflaer@gmail.com>
2018-05-30 19:20:46 +0500

    A little bit more functionality.

--- app/js/autocomplete.js
+++ app/js/autocomplete.js
@@ -17,7 +17,7 @@ function compare(full, part) {
 
 function cityPool() {
     let value = $('#filter-input').val().trim(),
-        matchedCities = [];
+        matchedCities = [],
         htmlText = '';
     if (value.length >= 0) {
         for (var i in json.responseJSON.Cities) {
@@ -32,7 +32,8 @@ function cityPool() {
     if (htmlText != 0) {
         filterOutput.innerHTML = htmlText;
     } else {
-        htmlText += '<div class="filter-output-none">Такого города нет, проверьте ввод.</div>';
+        htmlText += '<div id="filter-output-none" class="filter-output-none">Такого города нет, проверьте ввод.</div>';
+        window.setTimeout(wrongCity, 500);
         filterOutput.innerHTML = htmlText;
     }
     citySelect();
@@ -43,19 +44,30 @@ function citySelect() {
     $(".filter-output").on('click', function () {
         $("#filter-input").val($(this).html());
         $('div.filter-output').remove();
+        return true;
     });
 }
 
 function citiesClose(e) {
     $(document).mouseup(function (e) {
         let cities = $('#filter-output'),
-        noCities = $('.filter-output-none');
+            noCities = $('#filter-output-none');
         if (!cities.is(e.target) && cities.has(e.target).length === 0) {
             $('div.filter-output').remove();
-        } else if (!noCities.is(e.target) && noCities.has(e.target).length === 0) {
             $('div.filter-output-none').remove();
+            return true;
+        } else if (noCities.is(e.target)) {
+            $('div.filter-output-none').remove();
+            return true;
         }
     });
+}
+
+function wrongCity() {
+    $('div.filter-output').remove();
+    $('div.filter-output-none').remove();
+    htmlText = '<div id="filter-output-none" class="filter-output-wrong">Выберите значение из списка!</div>';
+    filterOutput.innerHTML = htmlText;
 }
 
 
